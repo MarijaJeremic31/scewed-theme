8811a5fd7a62afd9b8760f7215def58e4640158a
maja <maja@maja-PC>
2017-12-10 19:13:41 +0100

    dodat ispravljen counter

--- js/main.js
+++ js/main.js
@@ -1,116 +1,152 @@
-$(document).ready(function(){
-    
-$(window).scroll(function(){
-    var scroll = $(window).scrollTop();
-    if(scroll > 150){
-        $('header').addClass('py-2');
-    }else{
-        $('header').removeClass('py-2');
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 150) {
+            $('header').addClass('py-2');
+        } else {
+            $('header').removeClass('py-2');
+        }
+    });
+
+    $('.navbar-nav a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 2000);
+    });
+
+
+    if ($('.bxslider').length > 0) {
+        $('.lead-slider').bxSlider();
+        $('.testimonial-slider').bxSlider();
     }
-});
 
-$('.navbar-nav a').click(function(){
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 2000);
-});
+    function animation() {
+        var windowHeight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            var pozicija = $(this).offset().top;
+            var animacija = $(this).attr('data-animation');
+            if (pozicija < (scroll + windowHeight - 100)) {
+                $(this).addClass(animacija);
+            } else {
+                $(this).removeClass(animacija);
+            }
 
+        });
+    }
 
-if($('.bxslider').length > 0){
-    $('.lead-slider').bxSlider();
-    $('.testimonial-slider').bxSlider();
-}
+    animation();
 
-function animation(){
-         var windowHeight = $(window).height();
-         var scroll = $(window).scrollTop();
-         $('.animation').each(function(){
-             var pozicija = $(this).offset().top;
-             var animacija = $(this).attr('data-animation');
-             if(pozicija < (scroll + windowHeight - 100)){
-                $(this).addClass(animacija); 
-             }else{
-                $(this).removeClass(animacija);  
-             }
-         
-         });
-     }
-     
-     animation();
-     
-     $(window).scroll(function(){
-         animation();
-     });
+    $(window).scroll(function () {
+        animation();
+    });
 
-if($('.owl-carousel').length >0){
+    if ($('.owl-carousel').length > 0) {
         $('.team-slider').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:2
-        },
-        768:{
-            items:3  
-        },
-        992:{
-           items:4 
-        } 
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                }
+            }
+        });
+        $('.portfolio-slider').owlCarousel({
+            loop: true,
+            margin: 10,
+            dots: true,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                },
+                1200: {
+                    items: 5
+                }
+
+            }
+        });
     }
-});
-$('.portfolio-slider').owlCarousel({
-    loop:true,
-    margin:10,
-    dots:true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:2
-        },
-        768:{
-            items:3  
-        },
-        992:{
-           items:4 
-        },
-        1200:{
-           items:5 
-        }
-        
-    }
-});
-}
-$(window).scroll(function(){
-    var wh = $(window).height();
-    var scroll =$(window).scrollTop();
-    
-    $('.count').each(function () {
+    ;
+
+
+
+    var a = 0;
+    $(window).scroll(function () {
+
+        var oTop = $('.achievements').offset().top - window.innerHeight;
+        if (a === 0 && $(window).scrollTop() > oTop + 500) {
+            $('.counter-number').each(function () {
+                var $this = $(this),
+                        countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                    countNum: countTo
+                },
+                        {
+
+                            duration: 2000,
+                            easing: 'swing',
+                            step: function () {
+                                $this.text(Math.floor(this.countNum));
+                            },
+                            complete: function () {
+                                $this.text(this.countNum);
+                                //alert('finished');
+                            }
+
+                        });
+            });
+            a = 1;
+        };
+    }),
+            $(window).scroll(function () {
+        var wh = $(window).height();
+        var scroll = $(window).scrollTop();
         var position = $(this).offset().top;
-        if(position < wh + scroll - 100){
-            $(this).prop('Counter', 0).animate({
-             
-        Counter: $(this).text()
-    }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });  
-            
-        }
-  
-    
-});
-});
- 
- 
- 
+        $('.counter-number').each(function () {
+
+            if (position < wh + scroll - 100) {
+                $(this).prop('Counter', 0).animate({
+
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+
+            }
+
+
+        });
+    });
+
+
+
+
+
 });
